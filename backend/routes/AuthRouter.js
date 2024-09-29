3dcd72d26b0e3a4064efa9216acb61f97c210d4d
FouzanPS <mohammedfouzanmsd7@gmail.com>
2024-09-29 11:14:25 +0530

    Update2

--- backend/routes/AuthRouter.js
+++ backend/routes/AuthRouter.js
@@ -6,8 +6,8 @@ signup
 const router = Router()
 
 
-router.post('/login', loginValidation, login)
+router.post('/login', loginValidation, login) //loginvalidation will occur and then it will be sent to login in controller file.
 
-router.post('/signup', signupValidation, signup)
+router.post('/signup', signupValidation, signup) // same process for signup aswell
 
 export default router
